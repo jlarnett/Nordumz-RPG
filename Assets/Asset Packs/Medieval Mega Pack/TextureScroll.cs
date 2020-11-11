db4ab20f5df26fbc5f4b884816d46dde0f2efeab
Johnny Arnett <im10g@hotmail.com>
2020-11-10 23:59:21 -0500

    Game Stage

---
+++ Assets/Asset Packs/Medieval Mega Pack/TextureScroll.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace AquariusMax.Medieval
+{
+public class TextureScroll : MonoBehaviour {
+	public float scrollSpeed = 0.5F;
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+}
+}

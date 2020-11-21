67aa666ac290bf01c528c8fc749c5fa0a48e6bdc
Johnny Arnett <im10g@hotmail.com>
2020-11-21 16:20:53 -0500

    SourceTree Error Fix Get both version to a  stable point before ccontinueing

---
+++ Assets/Scripts/Inventories/MerchantInventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevTV.Inventories
+{
+    [CreateAssetMenu(menuName = ("RPG/Inventory/MerchantItemTable"))]
+    public class MerchantInventory : ScriptableObject
+    {
+        [SerializeField] private MerchantItemList[] ItemList;
+        [SerializeField] private string shopName;
+
+        [System.Serializable]
+        public class MerchantItemList
+        {
+            public MerchantItemHolder[] items;
+        }
+
+        [System.Serializable]
+        public class MerchantItemHolder
+        {
+            public InventoryItem item;
+            public int initialItemQuantity;
+            public int sellPrice;
+            public int BuyPrice;
+        }
+    }
+}
+

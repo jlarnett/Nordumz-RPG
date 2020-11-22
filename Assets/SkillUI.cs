93a3b75cf6ec7b091986c2145ca2350c3a477cca
Johnny Arnett <im10g@hotmail.com>
2020-11-21 22:45:21 -0500

    idk changes

---
+++ Assets/SkillUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Skill;
+using TMPro;
+using UnityEngine;
+
+
+namespace RPG.UI
+{
+    public class SkillUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI skillName;
+        [SerializeField] private TextMeshProUGUI skillLevel;
+
+        [SerializeField] private Skill.Skill skill;
+
+        private SkillHandler skillHandler;
+
+        void Awake()
+        {
+            skillHandler = GameObject.FindGameObjectWithTag("Player").GetComponent<SkillHandler>();
+        }
+
+        void Start()
+        {
+            SetupSkill();
+        }
+
+        private void SetupSkill()
+        {
+            int Level = skillHandler.GetLevelFromList(skill);
+
+            skillName.text = skill.ToString();
+            skillLevel.text = Level.ToString();
+        }
+    }
+}
+
